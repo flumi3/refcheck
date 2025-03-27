b2862a5140b7e5e99ed949598b6df732ccee6f3b
Sebastian Flum <sebastian.flum@zeiss.com>
2025-03-27 17:52:55 +0100

    improve color usage in stdout

--- refcheck/main.py
+++ refcheck/main.py
@@ -14,6 +14,7 @@ from refcheck.utils import (
     print_red_background,
     print_red,
     print_green,
+    print_yellow,
 )
 
 logger = logging.getLogger()
@@ -31,52 +32,54 @@ class ReferenceChecker:
 
     def check_references(self, references: list[Reference]):
         for ref in references:
-            logger.info(f"REFERENCE: {ref.__dict__}")
+            logger.info(ref)
 
             if ref.is_remote and not settings.check_remote:
                 logger.warning("Skipping remote reference check.")
-                continue
+                status = print_yellow("SKIPPED")
             elif ref.is_remote and settings.check_remote:
                 # Check if remote reference is reachable
                 try:
                     response = requests.head(ref.link, timeout=5, verify=False)
                     if response.status_code < 400:
-                        status = print_green_background("OK", self.no_color)
+                        status = print_green_background("OK")
                     else:
                         logger.info(f"Status code: {response.status_code}, Reason: {response.reason}")
-                        status = print_red_background("BROKEN", self.no_color)
+                        status = print_red_background("BROKEN")
                         self.broken_references.append(BrokenReference(**ref.__dict__, status=status))
                 except requests.exceptions.RequestException as e:
                     logger.error(f"Error: Could not reach remote reference '{ref.link}': {e}")
-                    status = print_red_background("BROKEN", self.no_color)
+                    status = print_red_background("BROKEN")
                     self.broken_references.append(BrokenReference(**ref.__dict__, status=status))
             else:
                 if ".md" in ref.link or "#" in ref.link:
                     if is_valid_markdown_reference(ref):
-                        status = print_green_background("OK", self.no_color)
+                        status = print_green_background("OK")
                     else:
-                        status = print_red_background("BROKEN", self.no_color)
+                        status = print_red_background("BROKEN")
                         self.broken_references.append(BrokenReference(**ref.__dict__, status=status))
                 else:
                     if file_exists(ref.file_path, ref.link):
-                        status = print_green_background("OK", self.no_color)
+                        status = print_green_background("OK")
                     else:
-                        status = print_red_background("BROKEN", self.no_color)
+                        status = print_red_background("BROKEN")
                         self.broken_references.append(BrokenReference(**ref.__dict__, status=status))
             print(f"{ref.file_path}:{ref.line_number}: {ref.syntax} - {status}")
 
     def print_summary(self):
         print("\nReference check complete.")
         print("\n============================| Summary |=============================")
+        print(print_green("[+] "))
+
 
         if self.broken_references:
-            print(print_red(f"[!] {len(self.broken_references)} broken references found:", self.no_color))
+            print(print_red(f"[!] {len(self.broken_references)} broken references found:"))
             self.broken_references = sorted(self.broken_references, key=lambda ref: (ref.file_path, ref.line_number))
 
             for broken_ref in self.broken_references:
                 print(f"{broken_ref.file_path}:{broken_ref.line_number}: {broken_ref.syntax}")
         else:
-            print(print_green("\U0001F389 No broken references.", self.no_color))
+            print(print_green("\U0001F389 No broken references."))
 
         print("====================================================================")
 
@@ -91,12 +94,12 @@ def main() -> bool:
 
     check_remote: bool = settings.check_remote
     if not check_remote:
-        print("[!] WARNING: Skipping remote reference check. Enable with arg --check-remote.")
+        print(print_yellow("[!] WARNING: Skipping remote reference check. Enable with arg --check-remote."))
 
     # Retrieve all markdown files specified by the user
     markdown_files = get_markdown_files_from_args(settings.paths, settings.exclude)
     if not markdown_files:
-        print("[!] No Markdown files specified or found.")
+        print(print_red("[!] No Markdown files specified or found."))
         return False
 
     print(f"\n[+] {len(markdown_files)} Markdown files to check.")
